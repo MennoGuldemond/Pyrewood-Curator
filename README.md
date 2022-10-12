70b6c1f8e5ec2efbcc7a84b4c5de447e9d31dd43
Menno Guldemond <mennoguldemond@live.nl>
2022-10-12 21:12:21 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+# Pyrewood Curator
+
+A discord bot that manages wow raid schedules and crafters information, stored in Google sheets.
+
+## Getting started
+
+1. run 'npm install'
+1. open https://developers.google.com/sheets/api/quickstart/nodejs and click the button 'Enable the Google Sheets API'
+1. Follow the steps to generate a credentials.json file and replace it with the one in the project (or add it to the root if there is none).
+1. run 'node generate-token.js' and follow the steps to create a token.json file.
+1. Add a config.json file to the root with the following properties:
+   - BotToken: string
+1. run 'node bot.js' to start the bot.
---
+++ utils/drive-reader.js
@@ -0,0 +1,99 @@
+const config = require('../config.json');
+const credentialsJson = require('../credentials.json');
+const tokenJson = require('../token.json');
+const { google } = require('googleapis');
+
+module.exports = {
+  getRaidData: async function () {
+    if (!credentialsJson) {
+      return console.error('Error loading credentials file');
+    }
+    const auth = authorize(credentialsJson);
+    return fetchRaidSheetData(auth);
+  },
+  getCrafterData: async function (faction) {
+    if (!credentialsJson) {
+      return console.error('Error loading credentials file');
+    }
+    const isHorde = faction.toLowerCase() === 'horde';
+    const auth = authorize(credentialsJson);
+    return fetchCrafterSheetData(auth, isHorde);
+  },
+};
+
+function authorize(credentials) {
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  if (!tokenJson) {
+    console.error('token.json not found, use generate-token.js to generate a token file.');
+    return null;
+  }
+  oAuth2Client.setCredentials(tokenJson);
+  return oAuth2Client;
+}
+
+async function fetchRaidSheetData(auth) {
+  const sheets = google.sheets({ version: 'v4', auth });
+  const result = await sheets.spreadsheets.values
+    .get({
+      spreadsheetId: config.raidSpreadsheetId,
+      range: 'A:J',
+    })
+    .then((result) => {
+      return result;
+    });
+
+  const rows = result.data.values;
+  const raidData = [];
+  if (rows.length) {
+    rows.map((row) => {
+      const rowData = {
+        day: row[0],
+        type: row[1],
+        faction: row[2],
+        raid: row[3],
+        time: row[4],
+        organizer: row[5],
+        link: row[6],
+        cut: row[7],
+        signUp: row[8],
+        sheet: row[9],
+      };
+      raidData.push(rowData);
+    });
+  } else {
+    console.error('No data found.');
+  }
+  return raidData;
+}
+
+async function fetchCrafterSheetData(auth, isHorde) {
+  const sheets = google.sheets({ version: 'v4', auth });
+  const sheetId = isHorde ? config.hordeCrafterSpreadsheetId : config.allianceCrafterSpreadsheetId;
+  const result = await sheets.spreadsheets.values
+    .get({
+      spreadsheetId: sheetId,
+      range: 'A:B',
+    })
+    .then((result) => {
+      return result;
+    });
+
+  const rows = result.data.values;
+  const crafterData = [];
+  if (rows.length) {
+    rows.map((row) => {
+      const rowData = {
+        item: row[0],
+        crafters: row[1],
+      };
+      crafterData.push(rowData);
+    });
+  } else {
+    console.error('No data found.');
+  }
+  crafterData.shift();
+  return crafterData;
+}
